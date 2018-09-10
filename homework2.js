33e811b3ef26de4302bddc860612b006cfa28f4d
minmaneh <43015690+minmaneh@users.noreply.github.com>
2018-09-10 08:53:58 +0400

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,12 @@
+	const m=10;
+	const n=9;
+	const p=90;
+	if(m+n===p){  
+  	console.log("You are right"+"12");
+	} else if(m-n===p){ 
+  	console.log("oops"+"try again");
+	} 
+	else { 
+   	console.log("Really?");
+	}
+
